23a6d37df8709d0ad58e32613469b6bfb9498a4c
atish23 <amaske@delaplex.in>
2016-04-18 17:12:00 +0530

    jquery images show

--- app/controllers/admin/accessories_controller.rb
+++ app/controllers/admin/accessories_controller.rb
@@ -31,9 +31,8 @@ def show
   @accessory = Accessory.find(params[:id])
   respond_to do |format|
     format.html
-    format.json {render json: @accessory.to_json(:only => [ :id,:acc_name, :price, :product_id, :variant ]), :methods => [:avatar_url] }
+    format.json {render json: @accessory.to_json(:only => [ :id,:acc_name, :price, :product_id, :variant ], :methods => [:avatar_url] )}
     #render :json => @model.to_json(:only => [:id,:name,:homephone,:cellphone])
-
   end
 end
 
