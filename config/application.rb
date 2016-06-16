574bd1c368c607965d9f9fc4acb749f5e4d40ba6
dchachada <dchachada@delaplex.in>
2016-06-16 15:46:42 +0530

    Error on new remote

--- config/application.rb
+++ config/application.rb
@@ -23,6 +23,7 @@ module Menguin
     # config.i18n.default_locale = :de
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
+     config.assets.initialize_on_precompile = false
      config.active_record.raise_in_transactional_callbacks = false
      config.active_job.queue_adapter = :delayed_job
     #### New added for custom error page 
